3239c7fa91fe8d117feb7d2b9ff4f670d30a0e33
CatUnderTheLeaf <catundertheleaf@gmail.com>
2022-12-23 14:57:42 +0100

    send service call for servo configure

--- robocar_ws/src/donkeycar/src/joy_teleop_node.py
+++ robocar_ws/src/donkeycar/src/joy_teleop_node.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 import sys
 import rospy
-# from i2cpwm_board.msg import ServoArray, Servo
-# from sensor_msgs.msg import Joy
+from i2cpwm_board.msg import ServoConfig
+from i2cpwm_board.srv import ServosConfig, ServosConfigRequest
+from i2cpwm_board.msg import ServoArray, Servo
+from sensor_msgs.msg import Joy
 
 class JoyToServoPublisher():
 
     def __init__(self):
         servo_config = rospy.get_param("servos")
         rospy.loginfo(servo_config)
-        
+        self.config_servos(servo_config)
+
         self.steering = 0
         self.steering_servo = servo_config[1]['servo']
         self.steering_axis = rospy.get_param("axis_angular")
@@ -20,45 +23,53 @@ class JoyToServoPublisher():
 
         self.offset = 0.03
 
-        # self.joy_sub = rospy.Subscriber(
-        #     rospy.get_param('~joy'),
-        #     Joy,
-        #     self.joy_callback,
-        #     queue_size=1)
+        self.joy_sub = rospy.Subscriber(
+             'joy',
+             Joy,
+             self.joy_callback,
+             queue_size=1)
        
-        # self.servo_pub = rospy.Publisher(
-        #     rospy.get_param('~servo_topic'),
-        #     ServoArray,
-        #     queue_size=1)
+        self.servo_pub = rospy.Publisher(
+             'servo_topic',
+             ServoArray,
+             queue_size=1)
 
         rospy.spin()
 
     def config_servos(self, config):
         rospy.wait_for_service('config_servos')
         try:
-            configure = rospy.ServiceProxy('config_servos', config_servos)
-            configure(config)
+            configure = rospy.ServiceProxy('config_servos', ServosConfig)
+            msg = ServosConfigRequest()
+            for servo in config:
+                serv = ServoConfig()
+                serv.servo = servo['servo']
+                serv.center = servo['center']
+                serv.range = servo['range']
+                serv.direction = servo['direction']
+                msg.servos.append(serv)
+            configure(msg)
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
 
-    # def joy_callback(self, data):
-    #     msg = ServoArray()
+    def joy_callback(self, data):
+        msg = ServoArray()
 
-    #     if (abs(data.axes[self.throttle_axis]-self.throttle) > self.offset):
-    #         self.throttle = data.axes[self.throttle_axis.axis]
-    #         serv = Servo()
-    #         serv.servo = self.throttle_servo
-    #         serv.value = self.throttle
-    #         msg.servos.append(serv)
-        
-    #     if (abs(data.axes[self.steering_axis]-self.steering) > self.offset):
-    #         self.steering = data.axes[self.steering_axis]
-    #         serv = Servo()
-    #         serv.servo = self.steering_servo
-    #         serv.value = self.steering
-    #         msg.servos.append(serv)
- 
-    #     self.servo_pub.publish(msg)
+        if (abs(data.axes[self.throttle_axis]-self.throttle) > self.offset):
+            self.throttle = data.axes[self.throttle_axis.axis]
+            serv = Servo()
+            serv.servo = self.throttle_servo
+            serv.value = self.throttle
+            msg.servos.append(serv)
+
+        if (abs(data.axes[self.steering_axis]-self.steering) > self.offset):
+            self.steering = data.axes[self.steering_axis]
+            serv = Servo()
+            serv.servo = self.steering_servo
+            serv.value = self.steering
+            msg.servos.append(serv)
+
+        self.servo_pub.publish(msg)
 
 def main(args):
     rospy.init_node('joy_teleop_node', anonymous=True, log_level=rospy.INFO)
